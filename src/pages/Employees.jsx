98fb2e80bfe85889a78466d1e03b1c6a154a0726
suryansh-git-hub <suryashukla233@gmail.com>
2026-06-19 17:29:37 +0530

    Added search and filter to employees

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -6,6 +6,10 @@ import Table from "../components/Table"
 function Employees() {
   const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedDepartment,
+       setSelectedDepartment] =
+       useState("All");
 
   useEffect(() => {
     fetch("https://dummyjson.com/users")
@@ -20,12 +24,69 @@ function Employees() {
     return <Loader />;
   }
 
+  const departments = [
+  "All",
+  ...new Set(
+    employees.map(
+      (employee) =>
+        employee.company.department
+    )
+  ),
+];
+
+ const filteredEmployees =
+  employees.filter((employee) => {
+    const fullName =
+      `${employee.firstName} ${employee.lastName}`
+        .toLowerCase();
+
+    const matchesSearch =
+      fullName.includes(
+        searchTerm.toLowerCase()
+      );
+
+    const matchesDepartment =
+      selectedDepartment === "All"
+        ? true
+        : employee.company.department ===
+          selectedDepartment;
+
+    return (
+      matchesSearch &&
+      matchesDepartment
+    );
+  });
+  
   return (
     <DashboardLayout>
       <h1>Employees</h1>
 
       <p>Total Employees: {employees.length}</p>
-      <Table employees={employees} />
+      <input  type="text" placeholder="Search employee"
+  value={searchTerm}
+  onChange={(e) =>
+    setSearchTerm(e.target.value)
+  }
+/>
+
+<select
+  value={selectedDepartment}
+  onChange={(e) =>
+    setSelectedDepartment(
+      e.target.value
+    )
+  }
+>
+  {departments.map((dept) => (
+    <option
+      key={dept}
+      value={dept}
+    >
+      {dept}
+    </option>
+  ))}
+</select>
+      <Table employees={filteredEmployees} />
     </DashboardLayout>
   );
 }
